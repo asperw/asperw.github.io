dd954ea63a5f05e5c420d647c740635b43880dd0
asperw <asperw@gmail.com>
2017-10-15 17:52:47 -0700

    Smaller + added clear

--- background2.js
+++ background2.js
@@ -2,7 +2,7 @@
 
 let x = 0;
 let y = 0;
-let spacing = 40;
+let spacing = 20;
 
 //called once on window load
 function setup(){
@@ -21,6 +21,7 @@ function draw(){
     //grid.display();
 
     stroke(255);
+
     if(random(1) < 0.5){
       line(x, y, x + spacing, y + spacing);
     }else{
@@ -40,4 +41,12 @@ function windowResized(){
     windowHeight = document.body.clientHeight;
 
     resizeCanvas(windowWidth, windowHeight);
+
+    x = y = 0;
+}
+
+function mousePressed(){
+  clear();
+
+  x = y = 0;
 }
